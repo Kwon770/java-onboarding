6612b00d471f224aff8428230dc7578b6b0fff98
Kwon770 <sckwon770@gmail.com>
2022-10-30 18:47:52 +0900

    feat: Impl getMaxScore

--- Features.md
+++ Features.md
@@ -1,5 +1,5 @@
 # Problem 1
-- [ ] ```getMaxScore(List<Integer>)``` 구현
+- [x] ```getMaxScore(List<Integer>)``` 구현
 - [ ] ```getNumberSumScore(String)``` 구현
 - [ ] ```getNumberMultiplyScore(String)``` 구현
 - [ ] solution()과 기능 함수 연결
--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+
+    private static int getMaxScore(List<Integer> scores) {
+        return Integer.max(getNumberSumScore(scores.get(0).toString()), getNumberMultiplyScore(scores.get(1).toString()));
+    }
+
+    private static int getNumberSumScore(String number) {
+        return 0;
+    }
+
+    private static int getNumberMultiplyScore(String number) {
+        return 0;
     }
 }
